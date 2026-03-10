60717a0551c54ad514fdaf2ea64aba63ef71cd65
shivusmith2405-netizen <shivusmith2405@gmail.com>
2026-03-10 20:08:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -228,4 +228,5 @@ def get_reviews(doctor_id):
 
     return jsonify(data)
 if __name__ == "__main__":
-     app.run(host="0.0.0.0",port=8080)
+    port=int(os.environ.get("port",5000))
+     app.run(host="0.0.0.0",port=port)
